4dbc47a6dbaa94a125672a2f31a0ddc4a4ae23fa
Verveyko Alexander <averveiko@gmail.com>
2020-10-04 14:56:56 +0300

    Реализация с использованием embedded mongo

--- 01/todo/src/main/resources/META-INF/sql/schema.sql
+++
@@ -1,8 +0,0 @@
-create table todo (
- id varchar(36) not null,
- description varchar(255) not null,
- created timestamp,
- modified timestamp,
- completed boolean,
- primary key (id)
-);
--- 02/app.groovy
+++
@@ -1,7 +0,0 @@
-@RestController
-class WebApp{
-	@GetMapping("/")
-	String welcome() {
-		"<h1><font face='vernada'>Spring Boot Rocks!</font></h1>"
-	}
-}
